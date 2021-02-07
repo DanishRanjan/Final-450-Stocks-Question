57675a461a1d4bed6e105f8cd91783287a5af59b
Danish Ranjan <ranjandanish@gmail.com>
2021-02-07 23:11:27 +0530

    Buy and Sell K Transaction Allowed , Highly Optimised

---
+++ buyAndSell_k_TransactionAllowedOptimised.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class buyAndSell_k_TransactionAllowedOptimised {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int k = sc.nextInt();
+		int[][] dp = new int[k + 1][n];
+		for (int t = 1; t <= k; t++) {
+			int max = Integer.MIN_VALUE;
+			for (int d = 1; d < n; d++) {
+				if (dp[t - 1][d - 1] - arr[d - 1] > max) {
+					max = dp[t - 1][d - 1] - arr[d - 1];
+				}
+
+				if (max + arr[d] > dp[t][d - 1]) {
+					dp[t][d] = max + arr[d];
+				} else {
+					dp[t][d] = dp[t][d - 1];
+				}
+			}
+		}
+		System.out.println(dp[k][n - 1]);
+	}
+}
